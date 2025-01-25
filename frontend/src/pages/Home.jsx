a8eaf356506e76240c5b19f10528c9beb18b7a46
Usman Khan <usmanjoiya981@gmail.com>
2025-01-25 14:47:33 +0500

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "http://localhost:8081/products";
+            const url = "https://deploy-mern2-6ldhj5y3j-muhammad-usman-khan-joiyas-projects.vercel.app/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
