f98066494d5305f0f4464a7822ff2b0339ea2cf1
Usman Khan <usmanjoiya981@gmail.com>
2025-01-25 14:46:40 +0500

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `http://localhost:8081/auth/login`;
+            const url = `https://deploy-mern2-6ldhj5y3j-muhammad-usman-khan-joiyas-projects.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
